28818722d71253a09423f402114067a6267ca4e3
kisamotoserum <arvindsnat@gmail.com>
2025-01-20 17:46:20 -0500

    made git sign column render to the right of line number column

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,7 +3,12 @@ return {
 	{
 		"lewis6991/gitsigns.nvim",
 		config = function()
-			require('gitsigns').setup()
+			require('gitsigns').setup{
+				signcolumn = auto,
+				on_attach = function()
+					vim.wo.signcolumn = "yes"
+				end
+			}
 		end,
 	},
 }
